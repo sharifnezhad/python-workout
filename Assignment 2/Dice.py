2f460c7aba794ddfbec8472950e036f3390a6994
Amir Hussein Sharifnezhad <83763809+sharifnezhad@users.noreply.github.com>
2021-08-04 16:57:41 +0430

    Update Dice.py

--- Assignment 2/Dice.py
+++ Assignment 2/Dice.py
@@ -1,8 +1,8 @@
 import random
-dice_number=random.randint(5,6)
+dice_number=random.randint(1,6)
 print('dice number:',dice_number)
 if dice_number==6:
     while dice_number==6:
-        dice_number=random.randint(5,6)
+        dice_number=random.randint(1,6)
         print('Award:',dice_number)
    
