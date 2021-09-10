fa63a3d121e03bf0d5a77cccd66f8b0329ba1a53
Amir Hussein Sharifnezhad <83763809+sharifnezhad@users.noreply.github.com>
2021-09-10 19:01:11 +0430

    Update square.py

--- Assignment11/square.py
+++ Assignment11/square.py
@@ -11,17 +11,19 @@ for i in range(10):
     for j in range(10):
         if i%2==0:
             if j%2==0:
-                arcade.draw_circle_filled(center_x+count,center_y,8,blue_color)
-            else:
-                arcade.draw_circle_filled(center_x+count, center_y, 8,red_color)
-        else:
-            if j%2==0:
                 arcade.draw_circle_filled(center_x+count,center_y,8,red_color)
             else:
                 arcade.draw_circle_filled(center_x+count, center_y, 8,blue_color)
+        else:
+            if j%2==0:
+                arcade.draw_circle_filled(center_x+count,center_y,8,blue_color)
+            else:
+                arcade.draw_circle_filled(center_x+count, center_y, 8,red_color)
         count+=20
     count=0
     center_y+=20
+
+
 arcade.finish_render()
 arcade.run()
 
