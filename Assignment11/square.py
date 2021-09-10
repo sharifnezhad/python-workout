c301c67ca104657c848d70c446d0668bb2f65088
Amir Hussein Sharifnezhad <83763809+sharifnezhad@users.noreply.github.com>
2021-09-10 16:17:37 +0430

    Add files via upload

---
+++ Assignment11/square.py
@@ -0,0 +1,27 @@
+import arcade
+arcade.Window(500,500,'Square')
+arcade.set_background_color(arcade.color.WHITE)
+red_color=arcade.color.RED
+blue_color=arcade.color.BLUE
+center_x=160
+center_y=160
+arcade.start_render()
+count=0
+for i in range(10):
+    for j in range(10):
+        if i%2==0:
+            if j%2==0:
+                arcade.draw_circle_filled(center_x+count,center_y,8,blue_color)
+            else:
+                arcade.draw_circle_filled(center_x+count, center_y, 8,red_color)
+        else:
+            if j%2==0:
+                arcade.draw_circle_filled(center_x+count,center_y,8,red_color)
+            else:
+                arcade.draw_circle_filled(center_x+count, center_y, 8,blue_color)
+        count+=20
+    count=0
+    center_y+=20
+arcade.finish_render()
+arcade.run()
+
