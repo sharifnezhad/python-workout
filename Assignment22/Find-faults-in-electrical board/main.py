2a7a3ac07f0549554e47febd59d238e11c9455c2
Amir Hussein Sharifnezhad <83763809+sharifnezhad@users.noreply.github.com>
2021-11-06 03:33:08 +0330

    Update main.py

--- Assignment22/Find-faults-in-electrical board/main.py
+++ Assignment22/Find-faults-in-electrical board/main.py
@@ -1,7 +1,8 @@
 import cv2
 img=cv2.imread('board-origin.bmp',0)
 img2=cv2.imread('board-test.bmp',0)
-result=cv2.subtract(img2,img)
-cv2.imwrite('result.jpg',result)
-cv2.imshow('result',result)
+img2=img2[::,::-1]
+result=cv2.absdiff(img2,img)
+cv2.imwrite('result2.jpg',result)
+cv2.imshow('result2',result)
 cv2.waitKey()
